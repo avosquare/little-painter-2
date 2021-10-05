5eea79a419cf0e6df59d1017d6d5d3bb4ee4042b
avosquare <akindelevictoropeyemi@gmail.com>
2021-10-05 15:51:53 +0200

    little painter 2

---
+++ map.js
@@ -0,0 +1,20 @@
+var mymap = L.map('mapid', {scrollWheelZoom: false}).setView([44.558837890625, 6.080686569213867], 10);
+
+var marker = L.marker([44.558837890625, 6.080686569213867]).addTo(mymap);
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(mymap);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	draggable: false,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
+
+mymap.dragging.disable();
